7db001edd3b8c76de3ea79320873ea386435c239
Eric Greene <eric@t4d.io>
2018-12-19 08:43:12 -0600

    small tweak to app component

--- template/src/App.js
+++ template/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to Training 4 Developers & WintellectNOW!
+            Welcome to Training 4 Developers & WintellectNOW!<br />
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
